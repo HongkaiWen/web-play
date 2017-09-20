142e14736b8beaed2a47f4539a490d60781cb501
hongkai <446410466@qq.com>
2017-09-20 10:11:44 +0800

    re build

---
+++ src/main/java/com/github/hongkaiwen/WebPlayApplication.java
@@ -0,0 +1,12 @@
+package com.github.hongkaiwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPlayApplication.class, args);
+	}
+}
---
+++ src/main/java/com/github/hongkaiwen/webplay/controller/IndexController.java
@@ -0,0 +1,64 @@
+package com.github.hongkaiwen.webplay.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * Created by hongkai on 2017/7/11.
+ */
+@Controller
+public class IndexController {
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/form")
+    public String form(HttpServletRequest request, Model model){
+        String i_test = request.getParameter("i_test");
+        if("info".equals(i_test)){
+            logger.info("info log output");
+        } else if("error".equals(i_test)){
+            logger.error("error log output");
+        }
+        System.out.println(i_test);
+        request.getSession(true).setAttribute("msg", i_test);
+        return "submit";
+    }
+
+    @RequestMapping("/ping")
+    @ResponseBody
+    public String ping(){
+        return "pong";
+    }
+
+    @RequestMapping("/302")
+    public String t302(){
+        return "302";
+    }
+
+    @RequestMapping("/forwardPage")
+    public String forwardPage(){
+        return "forwardPage";
+    }
+
+    @RequestMapping("/redirect302")
+    public String r302() throws IOException {
+        return "redirect:302";
+    }
+
+    @RequestMapping("/forward")
+    public String r301() throws IOException {
+        return "forward:forwardPage";
+    }
+}
